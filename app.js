f234e5f005a3d93d66bbb0daaecdad25429a3941
ZOHA ALI <zoha97069@gmail.com>
2026-05-30 09:30:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// problem # 01
+
+alert('wellcome to my website');
+
+//problem # 02
+
+alert('Error please enter a valid password');
+
+//problem # 03
+
+alert('Welcome to JS land... \nhappy coding');
+
+//problem #04
+
+alert('Wellcome to JS land...');
+alert('Happy Coding!');
+
+//probiem # 05
+
+alert("Hello.. I can run JSthrough my web browser's console.");
+
+//problem # 06
+
+alert('Hello ... Alert');
